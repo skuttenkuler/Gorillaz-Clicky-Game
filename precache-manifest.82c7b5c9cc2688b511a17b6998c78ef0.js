6961740dece38c39054c2ffa720bc243ebda7bd5
skuttenkuler <samkiddmusic@gmail.com>
2020-02-07 22:31:54 -0800

    Updates

---
+++ precache-manifest.82c7b5c9cc2688b511a17b6998c78ef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a3b95acf5472a3140d152ca8003ba3",
+    "url": "/Gorillaz-Clicky-Game/index.html"
+  },
+  {
+    "revision": "31c9df17a1793b5f6a00",
+    "url": "/Gorillaz-Clicky-Game/static/css/main.4a2a19d1.chunk.css"
+  },
+  {
+    "revision": "5c3f6388f5aaa66b6085",
+    "url": "/Gorillaz-Clicky-Game/static/js/2.e495034b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Gorillaz-Clicky-Game/static/js/2.e495034b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31c9df17a1793b5f6a00",
+    "url": "/Gorillaz-Clicky-Game/static/js/main.254cfe3a.chunk.js"
+  },
+  {
+    "revision": "071a3e54ed68f8f6647b",
+    "url": "/Gorillaz-Clicky-Game/static/js/runtime-main.90ca1626.js"
+  }
+]);
